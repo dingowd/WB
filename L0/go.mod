548f8cbfa0a2d14330ae027cffe8cac86ec2917b
dingowd <dingowd75@yandex.ru>
2022-08-08 16:41:35 +0300

    L0 postgres test ready

--- L0/go.mod
+++ L0/go.mod
@@ -19,6 +19,7 @@ require (
 	github.com/gofrs/uuid v4.2.0+incompatible // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/hashicorp/golang-lru v0.5.1 // indirect
 	github.com/jackc/fake v0.0.0-20150926172116-812a484cc733 // indirect
 	github.com/kr/pretty v0.1.0 // indirect
 	github.com/lib/pq v1.10.4 // indirect
--- L0/storage/postgres/postgres_test.go
+++ L0/storage/postgres/postgres_test.go
@@ -1,1 +1,39 @@
 package postgres
+
+import (
+	"github.com/dingowd/WB/L0/logger/lrus"
+	"github.com/dingowd/WB/L0/model"
+	_ "github.com/jackc/pgx/stdlib"
+	"github.com/jmoiron/sqlx"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func TestGetOrder(t *testing.T) {
+	var s Storage
+	s.DB, _ = sqlx.Open("pgx", "user=postgres dbname=WB sslmode=disable password=masterkey")
+	defer s.DB.Close()
+	s.Log = lrus.New()
+	item := model.Item{
+		ChrtId: 9934930,
+	}
+	items := make([]model.Item, 0)
+	items = append(items, item)
+	o := model.Order{
+		OrderUid:    "b563feb7b2b84b6test",
+		TrackNumber: "WBILMTESTTRACK",
+		CustomerId:  "test",
+		Items:       items,
+	}
+	err := s.CreateOrder(o)
+	require.Error(t, err)
+}
+
+func TestIsOrderExist(t *testing.T) {
+	var s Storage
+	s.DB, _ = sqlx.Open("pgx", "user=postgres dbname=WB sslmode=disable password=masterkey")
+	defer s.DB.Close()
+	s.Log = lrus.New()
+	ok := s.IsOrderExist("b563feb7b2b84b6test")
+	require.Equal(t, true, ok)
+}
