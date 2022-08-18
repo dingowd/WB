9b59c03d3b8cf049aca2e6a7b7aad31436cfe09a
dingowd <dingowd75@yandex.ru>
2022-08-18 20:29:58 +0300

    L2 11 middleware

--- L2/develop/dev11/internal/server/http/middleware.go
+++ L2/develop/dev11/internal/server/http/middleware.go
@@ -7,7 +7,7 @@ import (
 
 func loggingMiddleware(f http.HandlerFunc, logg logger.Logger) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		s := r.RequestURI
+		s := r.Method + " " + r.RequestURI
 		logg.Info(s)
 		f(w, r)
 	}
