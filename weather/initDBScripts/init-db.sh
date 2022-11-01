bab32a7e5315c852faeb8aad33e06666f0829e2b
dingowd <dingowd75@yandex.ru>
2022-11-01 23:07:27 +0300

    some docker changes

--- weather/initDBScripts/init-db.sh
+++ weather/initDBScripts/init-db.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
 
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+psql -v ON_ERROR_STOP=1 -U postgres -d postgres <<-EOSQL
     CREATE DATABASE weather;
 EOSQL
